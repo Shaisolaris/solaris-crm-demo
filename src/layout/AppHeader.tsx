cd377df19d47351c5b4beebc629f72a1d4aa1c76
Shai A <shaisolaris@users.noreply.github.com>
2026-04-10 06:55:37 -0700

    Replace TailAdmin logo in mobile AppHeader with Solaris brand mark

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -3,7 +3,6 @@ import { ThemeToggleButton } from "@/components/common/ThemeToggleButton";
 import NotificationDropdown from "@/components/header/NotificationDropdown";
 import UserDropdown from "@/components/header/UserDropdown";
 import { useSidebar } from "@/context/SidebarContext";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useState ,useEffect,useRef} from "react";
 
@@ -83,21 +82,13 @@ const AppHeader: React.FC = () => {
             {/* Cross Icon */}
           </button>
 
-          <Link href="/" className="lg:hidden">
-            <Image
-              width={154}
-              height={32}
-              className="dark:hidden"
-              src="./images/logo/logo.svg"
-              alt="Logo"
-            />
-            <Image
-              width={154}
-              height={32}
-              className="hidden dark:block"
-              src="./images/logo/logo-dark.svg"
-              alt="Logo"
-            />
+          <Link href="/" className="flex items-center gap-2 lg:hidden">
+            <span className="flex h-9 w-9 items-center justify-center rounded-lg bg-gradient-to-br from-orange-500 to-amber-500 text-base font-bold text-white">
+              S
+            </span>
+            <span className="text-lg font-semibold text-gray-900 dark:text-white">
+              Solaris CRM
+            </span>
           </Link>
 
           <button
